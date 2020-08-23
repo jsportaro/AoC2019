74a4055d191fca79d61e5ce19ff0af1dd70b5d3c
Joseph Portaro <jsportaro@outlook.com>
2020-08-23 08:18:38 -0400

    First night, second star

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
+int32_t fuel_by_weight(double weight)
+{
+    return floor((weight / 3.0F)) - 2;
+}
 
 int main(void) 
 {
@@ -17,11 +23,17 @@ int main(void)
         exit(1);
     }
 
+    double module_fuel = 0;
+    double fuel_fuel = 0;
     while ((read = getline(&fuel_count, &fuel_length, fuel)) != -1 )
     {   
-        printf("%s", fuel_count);
-
-        sum += (atof(fuel_count) / 3.0F) - 2;
+        double module_fuel = fuel_by_weight(atof(fuel_count));
+        sum += module_fuel;
+        fuel_fuel = module_fuel;
+        while ((fuel_fuel = fuel_by_weight(fuel_fuel)) > 0)
+        {
+            sum += fuel_fuel;
+        }
     }
 
     fclose(fuel);
